e40b494d3ee1dd5bb24febacaea3cdebae90cb3d
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-06-27 11:29:17 -0500

    feat(Ver. 0.2.0.0): :lipstick: Vue Server
    UX/UI
    
    Añadidas vistas vue, se ha creado un navbar y se ha modificado el contenido de la vista home

---
+++ ui/src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="py-2 bg-body-tertiary border-bottom">
+        <div class="container d-flex flex-wrap">
+            <ul class="nav me-auto">
+                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2 active"
+                        aria-current="page">Home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Features</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Pricing</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">FAQs</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">About</a></li>
+            </ul>
+            <ul class="nav">
+                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Login</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Sign up</a></li>
+            </ul>
+        </div>
+    </nav>
+    <header class="py-3 mb-4 border-bottom">
+        <div class="container d-flex flex-wrap justify-content-center">
+            <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
+                <svg class="bi me-2" width="40" height="32">
+                    <use xlink:href="#bootstrap" />
+                </svg>
+                <span class="fs-4">Stocket <i class="bi bi-archive-fill"></i> </span>
+            </a>
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
+                <input type="search" class="form-control" :value="props.dateTime" readonly>
+            </form>
+        </div>
+</header>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    dateTime: string
+}>()
+</script>
