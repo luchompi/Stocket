74ba722f60376564a0592580ca2a73124cc60747
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-06-27 12:30:39 -0500

    chore(Ver. 0.2.0.0): :truck: Vue Server
    Home and About Pages
    
    Se ha movido los archivos de home y about, ahora se encuentran dentro de la carpeta principal por mayor facilidad y organizacion de codigo

---
+++ ui/src/views/principal/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="card-body">
+    <div class="card-title">
+      <h1>Stocket</h1>
+    </div>
+    <p class="card-text">
+      Bienvenido a Stocket, una aplicación para el control de inventarios.
+    </p>
+
+  </div>
+</template>
