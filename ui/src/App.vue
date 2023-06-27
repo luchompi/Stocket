cd6b78f830282b0343de0a90a6aeeee7c5f2b2d8
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-06-27 12:34:20 -0500

    refactor(Ver. 0.2.0.0): :lipstick: Vue Server
    Views representation
    
    Se ha eliminado la tarjeta que contenia las vistas por mayor comodidad, ahora cada aplicación incluirá su propio layout

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -16,16 +16,7 @@ onMounted(()=>{
 
 
   <Navbar :dateTime="dateTime"/>
-  <div class="row justify-content-md-center">
-    <div class="col col-lg-10">
-      <div class="card">
-        <div class="card-header">Stocket <i class="bi bi-archive-fill"></i></div>
-        <div class="card-body">
-          <RouterView />
-        </div>
-      </div>
-    </div>
-  </div>
+  <RouterView />
 </template>
 
 
