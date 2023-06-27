bdd6c53bbf7bbffa8bb6c4b2bf0eb997abfc2a2b
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-06-27 12:35:20 -0500

    feat(Ver. 0.2.0.0): :sparkles: Vue Server
    Navbar Routes
    
    Se han habilitado las rutas a la aplicación principal, ahora al hacer clic sobre "Stocket" o sobre About se redireccionan las vistas correspondientes

--- ui/src/components/Navbar.vue
+++ ui/src/components/Navbar.vue
@@ -7,7 +7,7 @@
                 <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Features</a></li>
                 <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Pricing</a></li>
                 <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">About</a></li>
+                <li class="nav-item"><RouterLink :to="{name:'about'}" class="nav-link link-body-emphasis px-2">About</RouterLink></li>
             </ul>
             <ul class="nav">
                 <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Login</a></li>
@@ -17,12 +17,12 @@
     </nav>
     <header class="py-3 mb-4 border-bottom">
         <div class="container d-flex flex-wrap justify-content-center">
-            <a href="/" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
+            <RouterLink :to="{name:'home'}" class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
                 <svg class="bi me-2" width="40" height="32">
                     <use xlink:href="#bootstrap" />
                 </svg>
                 <span class="fs-4">Stocket <i class="bi bi-archive-fill"></i> </span>
-            </a>
+            </RouterLink>
             <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
                 <input type="search" class="form-control" :value="props.dateTime" readonly>
             </form>
