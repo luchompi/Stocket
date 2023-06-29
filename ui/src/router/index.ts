746ba8c839f863226eae305f9869a4ac51db30b8
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-06-29 14:48:20 -0600

    feat(Ver. 0.2.0.0): :sparkles: Vue Server
    Auth App Routes
    
    Se han añadido las rutas de auth al archivo maestro de rutas

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,3 +1,4 @@
+import authRoutes from '@/views/Auth/services/auth.routes'
 import principalRoutes from '@/views/principal/services/principal.routes'
 import { createRouter, createWebHistory } from 'vue-router'
 
@@ -5,6 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     ...principalRoutes,
+    ...authRoutes,
     
   ]
 })
