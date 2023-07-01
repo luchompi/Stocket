283997ca8d7512d2e46579ffa2344a1d423c3d44
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-06-30 20:16:40 -0500

    feat(Ver. 0.2.0.0): :alien: Vue Server
    User sesion Data
    
    Se ha invocado la funcion de consulta de datos de usurio del backend, ahora al iniciar sesión, el sistema automáticamente consulta los datos del perfil del usuario que ingresó

--- ui/src/views/Auth/services/auth.apis.ts
+++ ui/src/views/Auth/services/auth.apis.ts
@@ -1,11 +1,13 @@
 import baseApi from "@/apis/base.api";
 import { sesionStore } from "@/stores/sesion.store";
 import type { userData } from "./auth.interfaces";
+import { getUserData } from '@/views/Profile/services/profile.apis'
 
 export const getCredentials = async (data: any) => {
     const sesion = sesionStore()
     const response = await baseApi.post('jwt/create', data)
     sesion.setTokens(response.data)
+    getUserData()
     return response.status
 }
 
