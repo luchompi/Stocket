910538400d8ab16a580ce66fa1c367052a7a9ce5
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-06-30 20:14:00 -0500

    feat(Ver. 0.2.0.0): :lipstick: Vue Server
    Profile App views
    
    Se han añadido los archivos de vista para manipular el perfil de usuario, incluye LayOut, List y Update View

---
+++ ui/src/views/Profile/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row justify-content-md-center">
+        <div class="col col-lg-6">
+            <div class="card">
+                <div class="card-header">Perfil de usuario</div>
+                <div class="card-body">
+                    <router-view />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { loginRequired } from '@/hooks/sesion.hook';
+onMounted(() => {
+    loginRequired()
+})
+</script>
