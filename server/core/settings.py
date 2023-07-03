e8b2b3ac9bb0950c30ad6908e8e93f8126678f20
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-07-02 20:00:03 -0500

    fix(Ver. 0.1.0.0): :bug: Django Server
    DotEnv
    
    Se ha corregido error de uso de variables de entorno

--- server/core/settings.py
+++ server/core/settings.py
@@ -1,13 +1,13 @@
-from dotenv import load_dotenv
 from datetime import timedelta as td
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
