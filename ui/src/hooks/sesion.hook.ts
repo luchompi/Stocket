1a192f4135ce33e386a857812d8b0a6febb07cf0
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-07-11 23:08:22 -0500

    perf(Ver. 0.3.0.0): :zap: Vue server
    Customs hooks
    
    Se han actualizado los hooks de comprobacion de sesion y permisos, ahora son mas exactos, y realizan comprobaciones mas complejas

--- ui/src/hooks/sesion.hook.ts
+++ ui/src/hooks/sesion.hook.ts
@@ -1,19 +1,27 @@
-import { sesionStore } from "@/stores/sesion.store";
-import { useRouter } from "vue-router";
-import { watchEffect } from "vue";
+import {sesionStore} from "@/stores/sesion.store";
+import {useRouter} from "vue-router";
+import {watchEffect} from "vue";
 
-export const loginRequired = () =>{
+export const loginRequired = () => {
     const sesion = sesionStore()
     const url = useRouter()
     watchEffect(() => {
-        sesion.isLogged ? null : url.push({name:'login'})
+        sesion.isLogged ? null : url.push({name: 'login'})
     })
 }
 
-export const redirectToHome = () =>{
+export const loginAndAdminRequired = () => {
     const sesion = sesionStore()
     const url = useRouter()
     watchEffect(() => {
-        sesion.isLogged ? url.push({name:'home'}) : null
+        sesion.isLogged ? (sesion.UserData[0]?.is_superuser || sesion.UserData[0]?.groups?.includes('Administradores') ? null : url.push({name: 'Forbidden'})) : url.push({name: 'login'})
+    })
+}
+
+export const redirectToHome = () => {
+    const sesion = sesionStore()
+    const url = useRouter()
+    watchEffect(() => {
+        sesion.isLogged ? url.push({name: 'home'}) : null
     })
 }
