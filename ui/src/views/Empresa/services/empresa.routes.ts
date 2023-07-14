ef34a57b22e9189048e75045c8f8d17d3ce86915
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-07-14 17:26:14 -0500

    feat(Ver. 0.3.0.0): :sparkles: Vue server
    Sedes services
    
    Se han añadido las rutas completas para los procedimientos de las sedes

--- ui/src/views/Empresa/services/empresa.routes.ts
+++ ui/src/views/Empresa/services/empresa.routes.ts
@@ -2,6 +2,8 @@ import EmpresaLayout from '../EmpresaLayout.vue'
 import Home from '../main/Home.vue'
 import Create from '../main/Create.vue'
 import Update from '../main/Update.vue'
+import sedeRoutes from "@/views/Empresa/sede/services/sede.routes";
+
 const empresaRoutes = [
     {
         path: '/empresa',
@@ -10,18 +12,19 @@ const empresaRoutes = [
             {
                 path: '',
                 name: 'empresa',
-                component:Home
+                component: Home
             },
             {
-                path:'empresa/create',
-                name:'empresa-create',
-                component:Create
+                path: 'empresa/create',
+                name: 'empresa-create',
+                component: Create
             },
             {
-                path:'empresa/edit',
-                name:'empresa-edit',
-                component:Update
-            }
+                path: 'empresa/edit',
+                name: 'empresa-edit',
+                component: Update
+            },
+            ...sedeRoutes
         ]
     }
 ]
