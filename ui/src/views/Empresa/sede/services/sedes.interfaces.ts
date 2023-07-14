bd9d653955c698c93da4809b499ff39ae96bcaef
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-07-14 17:24:57 -0500

    feat(Ver. 0.3.0.0): :sparkles: Vue server
    Sedes Services
    
    Se han añadido apis,rutas e interface para las sedes

---
+++ ui/src/views/Empresa/sede/services/sedes.interfaces.ts
@@ -0,0 +1,8 @@
+export interface sedes {
+    name: string,
+    address: string,
+    phone: string,
+    email: string,
+    description: string,
+    web: string
+}
--- ui/src/views/Empresa/services/empresa.services.ts
+++ ui/src/views/Empresa/services/empresa.services.ts
@@ -1,8 +1,14 @@
 import baseApi from "@/apis/base.api";
-import type { empresaInterface } from "./empresa.interfaces";
-import { sesionStore } from "@/stores/sesion.store";
+import type {empresaInterface} from "./empresa.interfaces";
+import {sesionStore} from "@/stores/sesion.store";
+import type {sedes} from "@/views/Empresa/sede/services/sedes.interfaces";
 
+/**
+ * Estas son las funciones de api para empresa
+ * */
 export const getCompany = async () => {
+    const sesion = sesionStore()
+    baseApi.defaults.headers.common["Authorization"] = `JWT ${sesion.PAT}`;
     return await baseApi.get("empresa/");
 }
 
@@ -16,4 +22,38 @@ export const updateCompany = async (data: empresaInterface) => {
     const sesion = sesionStore()
     baseApi.defaults.headers.common["Authorization"] = `JWT ${sesion.PAT}`;
     return await baseApi.put(`empresa/${data.id}/`, data)
+}
+
+/**
+ * Estas son las apis para sedes
+ * */
+
+export const getSedes = async (NIT: string) => {
+    const sesion = sesionStore()
+    baseApi.defaults.headers.common["Authorization"] = `JWT ${sesion.PAT}`;
+    return await baseApi.get(`empresa/${NIT}/sedes/`)
+}
+
+export const storeSede = async (data: sedes, NIT: any) => {
+    const sesion = sesionStore()
+    baseApi.defaults.headers.common["Authorization"] = `JWT ${sesion.PAT}`;
+    return await baseApi.post(`empresa/${NIT}/sedes/`, data)
+}
+
+export const getSedeData = async (NIT: any, id: any) => {
+    const sesion = sesionStore()
+    baseApi.defaults.headers.common["Authorization"] = `JWT ${sesion.PAT}`;
+    return await baseApi.get(`empresa/${NIT}/sedes/${id}/`)
+}
+
+export const updateSede = async (data: sedes, NIT: any, id: any) => {
+    const sesion = sesionStore()
+    baseApi.defaults.headers.common["Authorization"] = `JWT ${sesion.PAT}`;
+    return await baseApi.put(`empresa/${NIT}/sedes/${id}/`, data)
+}
+
+export const deleteSede = async (NIT: any, id: any) => {
+    const sesion = sesionStore()
+    baseApi.defaults.headers.common["Authorization"] = `JWT ${sesion.PAT}`;
+    return await baseApi.delete(`empresa/${NIT}/sedes/${id}/`)
 }
