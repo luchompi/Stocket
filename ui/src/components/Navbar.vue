8069636c7c88fce0bcd3d788480b02d5bffd6f9d
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-07-18 16:10:34 -0500

    feat(Ver. 0.3.0.0): :sparkles: Vue server
    Empresa App
    
    Se ha añadido enlace a la aplicacion que permite añadir dependencias a las sedes

--- ui/src/components/Navbar.vue
+++ ui/src/components/Navbar.vue
@@ -2,16 +2,29 @@
   <nav class="py-2 bg-body-tertiary border-bottom">
     <div class="container d-flex flex-wrap">
       <ul class="nav me-auto">
-        <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2 active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2 active" aria-current="page">Home</a>
+        </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle link-dark" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle link-dark" href="#" role="button" data-bs-toggle="dropdown"
+             aria-expanded="false">
             Empresa <i class="bi bi-building"></i>
           </a>
           <ul class="dropdown-menu">
-            <li><RouterLink :to="{ name: 'empresa' }" class="dropdown-item">Ver datos <i class="bi bi-search"></i></RouterLink></li>
-            <li><RouterLink :to="{name:'dependencias'}" class="dropdown-item">Dependencias <i class="bi bi-plus-circle"></i></RouterLink></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li>
+              <RouterLink :to="{ name: 'empresa' }" class="dropdown-item">Ver datos <i class="bi bi-search"></i>
+              </RouterLink>
+            </li>
+            <li>
+              <RouterLink :to="{name:'dependencias'}" class="dropdown-item">Dependencias <i
+                  class="bi bi-plus-circle"></i></RouterLink>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li>
+              <RouterLink :to="{name:'operations-index'}" class="dropdown-item">Incluir dependencias en sedes <i class="bi bi-box-arrow-in-right"></i>
+              </RouterLink>
+            </li>
           </ul>
         </li>
         <li class="nav-item"><a href="#" class="nav-link link-body-emphasis px-2">Pricing</a></li>
@@ -24,7 +37,7 @@
         <li class="nav-item">
           <div class="dropdown">
             <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
-              data-bs-toggle="dropdown" aria-expanded="false">
+                    data-bs-toggle="dropdown" aria-expanded="false">
               Sesión iniciada como {{ sesion.UserData[0]?.username }}
             </button>
             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -54,9 +67,9 @@
   <header class="py-3 mb-4 border-bottom">
     <div class="container d-flex flex-wrap justify-content-center">
       <RouterLink :to="{ name: 'home' }"
-        class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
+                  class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
         <svg class="bi me-2" width="40" height="32">
-          <use xlink:href="#bootstrap" />
+          <use xlink:href="#bootstrap"/>
         </svg>
         <span class="fs-4">Stocket <i class="bi bi-archive-fill"></i> </span>
       </RouterLink>
@@ -68,7 +81,7 @@
 </template>
 
 <script setup lang="ts">
-import { sesionStore } from '@/stores/sesion.store';
+import {sesionStore} from '@/stores/sesion.store';
 
 const sesion = sesionStore()
 const props = defineProps<{
