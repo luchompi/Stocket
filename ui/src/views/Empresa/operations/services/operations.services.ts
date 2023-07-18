5b004098f46215780372aacb137964965b594646
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-07-18 17:22:00 -0500

    feat(Ver. 0.3.0.0): :sparkles: Vue server
    Empresa app
    
    Se ha creado servicio para consultar dependencias por sede y, para eliminar dependencias por sedes

--- ui/src/views/Empresa/operations/services/operations.services.ts
+++ ui/src/views/Empresa/operations/services/operations.services.ts
@@ -6,3 +6,15 @@ export const saveDependenciasBySede =  (sede:any,data:any) => {
     baseApi.defaults.headers.common['Authorization'] = `JWT ${sesion.PAT}`
     return baseApi.post(`empresa/sedes/${sede}/dependencias/`, data)
 }
+
+export const getDependenciasBySede = (sede:any) => {
+    const sesion = sesionStore()
+    baseApi.defaults.headers.common['Authorization'] = `JWT ${sesion.PAT}`
+    return baseApi.get(`empresa/sedes/${sede}/dependencias/`)
+}
+
+export const deleteDependenciaBySede = (sede:any,dependencia:any) => {
+    const sesion = sesionStore()
+    baseApi.defaults.headers.common['Authorization'] = `JWT ${sesion.PAT}`
+    return baseApi.delete(`empresa/sedes/${sede}/dependencias/${dependencia}/`)
+}
