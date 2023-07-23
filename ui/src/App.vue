d5f420c9392101731cd52580a0925e323af63a42
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-07-23 18:02:10 -0500

    feat(Ver. 0.3.0.0): :lipstick: Vue Server
    System alerts
    
    Se ha configurado alertas en sistema, ahora se notifica cuando los datos de la entidad no se han añadido

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,34 +1,37 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from '@/components/Navbar.vue'
-import {ref,onMounted,watchEffect} from 'vue'
+import { ref, onMounted, watchEffect } from 'vue'
 import { sesionStore } from './stores/sesion.store';
 const dateTime = ref<string>('')
 const sesion = sesionStore()
 const company = ref<boolean>(false)
-onMounted(()=>{
-  setInterval(()=>{
+onMounted(() => {
+  setInterval(() => {
     dateTime.value = new Date().toLocaleString('es-co')
     sesion.incrementTimer()
-    watchEffect(()=>{
+    watchEffect(() => {
       company.value = !!sesion.NIT
     })
-  },1000)
+  }, 1000)
 })
 
 </script>
 
 <template>
-<div @mousemove="sesion.refreshAccessToken">
-  <Navbar :dateTime="dateTime"/>
-  <div class="alert alert-warning alert-dismissible fade show" role="alert" v-if="sesion.isLogged && (sesion.UserData[0]?.groups?.includes('Administradores') || sesion.UserData[0]?.is_superuser)">
-    <strong>¡Atención!</strong> Esta cuenta posee permisos de administrador. Tenga cuidado con los cambios que realice, ya que no se pueden deshacer.
+  <div @mousemove="sesion.refreshAccessToken">
+    <Navbar :dateTime="dateTime" />
+    <div class="alert alert-warning alert-dismissible fade show" role="alert"
+      v-if="sesion.isLogged && (sesion.UserData[0]?.groups?.includes('Administradores') || sesion.UserData[0]?.is_superuser)">
+      <strong>¡Atención!</strong> Esta cuenta posee permisos de administrador. Tenga cuidado con los cambios que realice,
+      ya que no se pueden deshacer.
+    </div>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert" v-if="!company && sesion.isLogged">
+      <strong>¡Alerta!</strong> No hay datos de la empresa. Por favor, comuníquese con el administrador del sistema para
+      que los ingrese.
+    </div>
+    <RouterView />
   </div>
-  <div class="alert alert-danger alert-dismissible fade show" role="alert" v-if="!company && sesion.isLogged">
-    <strong>¡Alerta!</strong> No hay datos de la empresa. Por favor, comuníquese con el administrador del sistema para que los ingrese.
-  </div>
-  <RouterView />
-</div>
 </template>
 
 
