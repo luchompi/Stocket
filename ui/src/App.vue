9f0375ea46f49a25ede82ac789a094f258389eb2
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-07-26 15:21:39 -0500

    fix(Ver. 0.3.0.0): :bug: Vue server
    Stocket token management
    
    Se ha corregido el error de no actualizacion de tokens de acceso

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterView} from 'vue-router'
 import Navbar from '@/components/Navbar.vue'
-import { ref, onMounted, watchEffect } from 'vue'
-import { sesionStore } from './stores/sesion.store';
+import {onMounted, ref, watchEffect} from 'vue'
+import {sesionStore} from './stores/sesion.store';
+
 const dateTime = ref<string>('')
 const sesion = sesionStore()
 const company = ref<boolean>(false)
@@ -19,18 +20,19 @@ onMounted(() => {
 </script>
 
 <template>
-  <div @mousemove="sesion.refreshAccessToken">
-    <Navbar :dateTime="dateTime" />
+  <div @mousemove="sesion.refreshAccessToken()">
+    <Navbar :dateTime="dateTime"/>
     <div class="alert alert-warning alert-dismissible fade show" role="alert"
-      v-if="sesion.isLogged && (sesion.UserData[0]?.groups?.includes('Administradores') || sesion.UserData[0]?.is_superuser)">
-      <strong>¡Atención!</strong> Esta cuenta posee permisos de administrador. Tenga cuidado con los cambios que realice,
+         v-if="sesion.isLogged && (sesion.UserData[0]?.groups?.includes('Administradores') || sesion.UserData[0]?.is_superuser)">
+      <strong>¡Atención!</strong> Esta cuenta posee permisos de administrador. Tenga cuidado con los cambios que
+      realice,
       ya que no se pueden deshacer.
     </div>
     <div class="alert alert-danger alert-dismissible fade show" role="alert" v-if="!company && sesion.isLogged">
       <strong>¡Alerta!</strong> No hay datos de la empresa. Por favor, comuníquese con el administrador del sistema para
       que los ingrese.
     </div>
-    <RouterView />
+    <RouterView/>
   </div>
 </template>
 
