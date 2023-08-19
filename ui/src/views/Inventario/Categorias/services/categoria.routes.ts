e2dd7e779d3797b66c8b085e8947c5226bd061b8
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-08-18 21:59:25 -0500

    fix(Ver. 0.4.0.0): :bug: Vue server
    Inventario apps - Bugs
    
    Se han solucionado bugs

--- ui/src/views/Inventario/Categorias/services/categoria.routes.ts
+++ ui/src/views/Inventario/Categorias/services/categoria.routes.ts
@@ -1,5 +1,5 @@
 import CategoriasLayout from '../CategoriasLayout.vue'
-import ListView from '../components/ListView.vue'
+import  ListView  from '../components/ListView.vue'
 import CreateView from '../components/CreateView.vue'
 import UpdateView from '../components/UpdateView.vue'
 const categoriasRoutes =[
