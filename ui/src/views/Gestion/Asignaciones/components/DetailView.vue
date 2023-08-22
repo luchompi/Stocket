5f404cbb38d3eb587023b6de906191a70637baf5
lpich <lpichon63@gmail.com>
2023-08-22 12:55:18 -0500

    feat(Ver. 0.5.0.0): :sparkles: Vue server
    Asignaciones
    
    Se ha creado vista de detalles de asignacion

---
+++ ui/src/views/Gestion/Asignaciones/components/DetailView.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import {getAsignacion, getElementsinAsignment} from "../services/asignacion.services";
+import {useRouter} from "vue-router";
+import type {Asignation, AsignationDetail} from "../services/asignacion.interfaces";
+import ElementView from './ElementView.vue'
+
+const asignacion = ref({} as Asignation)
+const funcionario = ref([] as any)
+const details = ref([] as AsignationDetail[])
+const url = useRouter()
+
+const getElements = async () => {
+  const response = await getElementsinAsignment(url.currentRoute.value.params.id)
+  details.value = response.data
+}
+const getData = async () => {
+  const response = await getAsignacion(url.currentRoute.value.params.id)
+  asignacion.value = response.data
+  funcionario.value = response.data.funcionario
+  getElements()
+}
+onMounted(() => {
+  getData()
+})
+</script>
+
+<template>
+  <div class="card-body">
+    <h6>Detalles de asignacion con PID {{ asignacion.id }}</h6>
+    <div class="row">
+      <p>Proceso realizado del {{ asignacion.timestamps }}</p>
+      <h4>Funcionario: <strong>{{ funcionario[0]?.iden }}</strong> - {{ funcionario[0]?.first_name }} {{
+          funcionario[0]?.last_name
+        }}</h4>
+      <h4>Elementos asignados</h4>
+      <div v-for="detail in details" :key="detail.id">
+        <div v-for="item in detail.elementos">
+          <div class="card">
+            <div class="card-body">
+              <ElementView :element="item"/>
+            </div>
+          </div>
+        </div>
+        <br>
+      </div>
+    </div>
+  </div>
+</template>
