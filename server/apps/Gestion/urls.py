8836ce06907ceaffe704962de9db4b97e268c32a
luchompi <lpichon63@gmail.com>
2023-08-25 22:24:49 -0500

    feat(Ver. 0.5.0.0): ✨Django server - Gestion app
    Se han añadido apis, consultas y demás para las asignaciones

--- server/apps/Gestion/urls.py
+++ server/apps/Gestion/urls.py
@@ -4,10 +4,19 @@ from rest_framework.urlpatterns import format_suffix_patterns as fsp
 from . import views
 
 urlpatterns = [
+    ###APIs de Asignaciones
+    #Lista de asignaciones y las crea
     path('asignaciones/', views.AsignacionIndex.as_view()),
+    #Filtrado de asignaciones por funcionario o id
     path('asignaciones/filter/<int:pk>/', views.AsignacionSearch.as_view()),
+    #obtener una asignacion por id
     path('asignaciones/details/<int:pk>/', views.AsignacionDetail.as_view()),
+    #obtener elementos de una asignacion
     path('asignaciones/detalles/<int:pk>/', views.DetallesAsignacionDetail.as_view()),
+    #obtener elementos disponibles para asignar
+    path('asignaciones/detalles/disponibles/', views.DetalleAsignacionSearch.as_view()),
+    #obetenr elementos disponibles para asignar por argumentos
+    path('asignaciones/detalles/disponibles/args/', views.SearchElementByArgs.as_view()),
 ]
 
 urlpatterns = fsp(urlpatterns)
--- server/apps/Gestion/views.py
+++ server/apps/Gestion/views.py
@@ -3,42 +3,85 @@ from django.db.models import Q
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from apps.Inventario.models import Elemento
+from apps.Inventario.serializers import ElementoViewSerializer
 from .models import Asignacion, DetallesAsignacion
 from .serializers import AsignacionSerializer, DetallesAsignacionSerializer
+from django.db.models import Q
+from django.db import transaction
 
 
+"""
+    Esta clase se encarga de listar y guardar asignaciones
+"""
 class AsignacionIndex(APIView):
     permission_classes = [isAdminOrSuperuser | isEncargado]
-
     def get(self, request):
-        asignacion = Asignacion.objects.order_by('timestamps')[:5]
+        asignacion = Asignacion.objects.order_by('-timestamps')[:5]
         serializer = AsignacionSerializer(asignacion, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
+    
+    def post(self, request, format=None):
+        funcionario = request.data['funcionario']
+        elementos = request.data['elementos']
+        asignacion = Asignacion.objects.create(funcionario_id=funcionario['iden'], user=request.user)
+        with transaction.atomic():
+            try:
+                for elemento in elementos:
+                    elemento_obj = Elemento.objects.get(placa=elemento['placa'])
+                    DetallesAsignacion.objects.create(asignacion=asignacion, elemento=elemento_obj)
+                    elemento_obj.estado = 'Asignado'
+                    elemento_obj.save()
+            except Exception as e:
+                transaction.rollback()
+                return Response({'message': e}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = AsignacionSerializer(asignacion)
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
 
 class AsignacionDetail(APIView):
     permission_classes = [isAdminOrSuperuser | isEncargado]
-
     def get(self, request, pk, format=None):
         asignacion = Asignacion.objects.get(pk=pk)
         serializer = AsignacionSerializer(asignacion)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
 class AsignacionSearch(APIView):
     permission_classes = [isAdminOrSuperuser | isEncargado]
-
     def get(self, request, pk):
         asignacion = Asignacion.objects.filter(Q(funcionario__iden=pk) | Q(funcionario__first_name__icontains=pk))
         serializer = AsignacionSerializer(asignacion, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
 class DetallesAsignacionDetail(APIView):
     permission_classes = [isAdminOrSuperuser | isEncargado]
-
     def get(self, request, pk, format=None):
         queryset = DetallesAsignacion.objects.filter(asignacion_id=pk)
         serializer = DetallesAsignacionSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+class DetalleAsignacionSearch(APIView):
+    permission_classes = [isAdminOrSuperuser | isEncargado]
+    def get(self, request, format=None):
+        queryset = Elemento.objects.filter(estado='Por asignar')[:5]
+        serializer = ElementoViewSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+""""
+    Busqueda de elementos por argumentos, esta busqueda solo aplica para la consulta
+    de elementos cuando se va a realizar una asignacion, ya sea por marca, por referencia,
+    por modelo o por placa"""
+class SearchElementByArgs(APIView):
+    def post(self,request,format=None):
+        query = Q(estado='Por asignar')
+        if request.data['param'] == 'marca':
+            query &= Q(referencia__marca__name__icontains=request.data['value'])
+        elif request.data['param'] == 'categoria':
+            query &= Q(referencia__categoria__name__icontains=request.data['value'])
+        elif request.data['param'] == 'modelo':
+            query &= Q(modelo__icontains=request.data['value'])
+        elif request.data['param'] == 'placa':
+            query &= Q(placa__icontains=request.data['value'])
+        queryset = Elemento.objects.filter(query)[:5]
+        serializer = ElementoViewSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
