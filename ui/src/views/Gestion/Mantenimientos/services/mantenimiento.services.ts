f13de723c8b6861ef3516a5e5fd3f20b9b019c09
luchompi <lpichon63@gmail.com>
2023-09-05 14:00:52 -0500

    feat(Ver. 0.5.0.0): :sparkles: Vue server - Gestion app
    Mantenimiento
    
    Se han escrito los servicios para mantenimiento

---
+++ ui/src/views/Gestion/Mantenimientos/services/mantenimiento.services.ts
@@ -0,0 +1,34 @@
+import baseApi from "@/apis/base.api";
+import { sesionStore } from "@/stores/sesion.store";
+
+export const obtenerMantenimientos = async () =>{
+    return await baseApi.get('gestion/mantenimientos/', {
+        headers: {
+            Authorization: `JWT ${sesionStore().PAT}`
+        }
+    })
+} 
+
+export const crearMantenimiento = async(elemento:any,data:any) =>{
+    return await baseApi.post(`gestion/mantenimientos/create/${elemento}/`,data, {
+        headers: {
+            Authorization: `JWT ${sesionStore().PAT}`
+        }
+    })
+}
+
+export const obtenerMantenimiento = async(id:any) =>{
+    return await baseApi.get(`gestion/mantenimientos/details/${id}/`, {
+        headers: {
+            Authorization: `JWT ${sesionStore().PAT}`
+        }
+    })
+}
+
+export const actualizarMantenimiento = async(id:any,data:any) =>{
+    return await baseApi.put(`gestion/mantenimientos/details/${id}/`,data, {
+        headers: {
+            Authorization: `JWT ${sesionStore().PAT}`
+        }
+    })
+}
