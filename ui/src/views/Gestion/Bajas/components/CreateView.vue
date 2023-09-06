37886f97caad50615ab64558c4386dcd3fc6ef14
luchompi <lpichon63@gmail.com>
2023-09-05 21:57:16 -0500

    feat(Ver. 0.5.0.0): :sparkles: Vue server - Gestion app
    Bajas
    
    Se han añadido vistas de proceso de baja

---
+++ ui/src/views/Gestion/Bajas/components/CreateView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import elementsContainer from './containers/elementsContainer.vue'
+import storeContainer from './containers/storeContainer.vue'
+</script>
+
+<template>
+  <h4 class="card-title">Crear nueva baja </h4>
+  <div class="card-text">
+    <div class="row">
+      <div class="col">
+        <elementsContainer/>
+      </div>
+      <div class="col">
+        <storeContainer/>
+      </div>
+    </div>
+  </div>
+</template>
