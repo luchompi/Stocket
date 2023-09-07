6cca0c9c7e5199629a7cc09881f4f01995d9514a
luchompi <lpichon63@gmail.com>
2023-09-07 12:31:57 -0500

    fix(Ver. 0.5.0.0): ✨Vue server - Gestion app
    Consulta - Mantenimientos y bajas
    
    se ha alterado la represntación de home para mostrar reportes

--- ui/src/views/principal/PrincipalLayout.vue
+++ ui/src/views/principal/PrincipalLayout.vue
@@ -1,11 +1,7 @@
 <template>
-    <div class="row justify-content-md-center">
-        <div class="col col-lg-6">
-            <div class="card">
-              <div class="card-body">
-                <router-view />
-              </div>
-            </div>
-        </div>
-    </div>    
+  <div class="row justify-content-md-center">
+    <div class="col col-lg-10">
+      <router-view/>
+    </div>
+  </div>
 </template>
