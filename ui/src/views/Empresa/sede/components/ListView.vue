2a734f09a6264cdbf93c9bc07ee4ddcb9a35a293
luchompi <lpichon63@gmail.com>
2023-09-07 15:59:24 -0500

    refactor(Ver. 0.6.0.0 - beta): :sparkles: Vue server - Empresa app
    Refactor
    
    Se ha refactorizado codigo, ahora puede leerse mas claro

--- ui/src/views/Empresa/sede/components/ListView.vue
+++ ui/src/views/Empresa/sede/components/ListView.vue
@@ -3,10 +3,10 @@
     <div>
       <h6>Estas son las sedes en sistema</h6>
       <div class="input-group mb-3">
-          <span class="input-group-text" id="inputSearch"><i class="bi bi-search"></i></span>
-          <input type="text" class="form-control" placeholder="Ingrese nombre de sede a buscar"
-            aria-label="Username" aria-describedby="inputSearch" v-model="search" />
-        </div>
+        <span class="input-group-text" id="inputSearch"><i class="bi bi-search"></i></span>
+        <input type="text" class="form-control" placeholder="Ingrese nombre de sede a buscar"
+               aria-label="Username" aria-describedby="inputSearch" v-model="search"/>
+      </div>
     </div>
     <div v-if="loading">
       Espere...
@@ -21,33 +21,35 @@
       <div v-else>
         <table class="table table-striped table-hover">
           <thead>
-            <tr>
-              <th>ID</th>
-              <th>Nombre</th>
-              <th>Opciones</th>
-            </tr>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Opciones</th>
+          </tr>
           </thead>
           <tbody>
-            <tr v-for="element in queryset" :key="element.id">
-              <td>
-                {{ element.id }}
-              </td>
-              <td>
-                {{ element.name }}
-              </td>
-              <td>
-                <div class="btn-group" role="group" aria-label="Basic example">
-                  <RouterLink :to="{ name: 'sedes-detail', params: { nit: url.currentRoute.value.params.nit, id: element.id } }"
+          <tr v-for="element in queryset" :key="element.id">
+            <td>
+              {{ element.id }}
+            </td>
+            <td>
+              {{ element.name }}
+            </td>
+            <td>
+              <div class="btn-group" role="group" aria-label="Basic example">
+                <RouterLink
+                    :to="{ name: 'sedes-detail', params: { nit: url.currentRoute.value.params.nit, id: element.id } }"
                     type="button" class="btn btn-info">
-                    Detalles <i class="bi bi-search"></i>
-                  </RouterLink>
-                  <RouterLink :to="{ name: 'sedes-update', params: { nit: url.currentRoute.value.params.nit, id: element.id } }"
+                  Detalles <i class="bi bi-search"></i>
+                </RouterLink>
+                <RouterLink
+                    :to="{ name: 'sedes-update', params: { nit: url.currentRoute.value.params.nit, id: element.id } }"
                     type="button" class="btn btn-warning">Editar <i class="bi bi-pencil-square"></i></RouterLink>
-                  <button type="button" class="btn btn-danger" @click="eliminarSede(element.id)">Eliminar <i
-                      class="bi bi-trash"></i></button>
-                </div>
-              </td>
-            </tr>
+                <button type="button" class="btn btn-danger" @click="eliminarSede(element.id)">Eliminar <i
+                    class="bi bi-trash"></i></button>
+              </div>
+            </td>
+          </tr>
           </tbody>
         </table>
       </div>
@@ -55,9 +57,9 @@
   </div>
 </template>
 <script setup lang="ts">
-import { ref, watchEffect } from "vue";
-import { deleteSede, getSedes, searchSede } from "@/views/Empresa/services/empresa.services";
-import { useRouter } from 'vue-router';
+import {ref, watchEffect} from "vue";
+import {deleteSede, getSedes, searchSede} from "@/views/Empresa/services/empresa.services";
+import {useRouter} from 'vue-router';
 import Swal from "sweetalert2";
 
 const search = ref<string>('')
@@ -68,46 +70,44 @@ const queryset = ref<any>([])
 const searchData = async () => {
   loading.value = true
   await searchSede(search.value)
-    .then((Response) => {
-      queryset.value = Response.data
-    })
-    .finally(() => {
-      loading.value = false
-    })
+      .then((Response) => {
+        queryset.value = Response.data
+      })
+      .finally(() => {
+        loading.value = false
+      })
 }
 
 const getData = async () => {
   loading.value = true
-  const NIT = url.currentRoute.value.params.nit
-  await getSedes(NIT.toString())
-    .then((Response) => {
-      queryset.value = Response.data
-    })
-    .finally(() => {
-      loading.value = false
-    })
+  await getSedes(url.currentRoute.value.params.nit)
+      .then((Response) => {
+        queryset.value = Response.data
+      })
+      .finally(() => {
+        loading.value = false
+      })
 }
 
 watchEffect(() => {
   if (!search.value) {
     getData()
-  }
-  else {
+  } else {
     searchData()
   }
 })
 
-const eliminarSede = (id:any) => {
+const eliminarSede = (id: any) => {
   Swal.fire({
-    icon:'question',
-    title:'¿Está seguro?',
-    text:'Este proceso no se puede deshacer',
-    showCancelButton:true,
-    showConfirmButton:true,
-    confirmButtonText:'Sí, continuar',
-    cancelButtonText:'No, cancelar'
-  }).then((results)=>{
-    if(results.isConfirmed){
+    icon: 'question',
+    title: '¿Está seguro?',
+    text: 'Este proceso no se puede deshacer',
+    showCancelButton: true,
+    showConfirmButton: true,
+    confirmButtonText: 'Sí, continuar',
+    cancelButtonText: 'No, cancelar'
+  }).then((results) => {
+    if (results.isConfirmed) {
       proceed(id)
     }
   })
