8a64ff64ce2f6af6dc11087a00462795945e7fd7
Luis Pichon <lpichong@misena.edu.co>
2023-09-21 15:56:33 -0500

    fix(Ver. 0.6.0.0 - beta): :lipstick: Stocket - Bug 28
    UI - Empresa
    
    Se han añadido botones, animaciones, etc a vistas

--- ui/src/views/Empresa/main/Update.vue
+++ ui/src/views/Empresa/main/Update.vue
@@ -1,36 +1,44 @@
 <template>
-  <div class="d-flex align-items-center">
-    <h5 class="text-center flex-grow-1 mb-0">Actualizar datos de empresa</h5>
-  </div>
-  <div class="row">
-    <p>Ingrese los datos a modificar, luego haga clic en <strong>guardar</strong></p>
-    <div class="col">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Datos actuales de la entidad</h5>
-          <DataComponent :empresa="queryset"/>
+  <div v-if="loading"></div>
+  <div v-else>
+    <div class="d-flex align-items-center">
+      <h5 class="text-center flex-grow-1 mb-0">Actualizar datos de empresa</h5>
+      <RouterLink :to="{name:'empresa'}" type="button" class="btn btn-secondary">Atrás <i class="bi bi-arrow-left"></i></RouterLink>
+    </div>
+    <div class="row">
+      <p>Ingrese los datos a modificar, luego haga clic en <strong>guardar</strong></p>
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Datos actuales de la entidad</h5>
+            <DataComponent :empresa="queryset" />
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col">
-      <formComponent :empresa="queryset" @onUpdate="onUpdate"/>
+      <div class="col">
+        <formComponent :empresa="queryset" @onUpdate="onUpdate" />
+      </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import {onMounted, ref} from 'vue'
-import type {empresaInterface} from '../services/empresa.interfaces';
-import {getCompany, updateCompany} from '../services/empresa.services';
+import { onMounted, ref } from 'vue'
+import type { empresaInterface } from '../services/empresa.interfaces';
+import { getCompany, updateCompany } from '../services/empresa.services';
 import formComponent from './components/formComponent.vue'
 import DataComponent from './components/dataComponent.vue';
 import Swal from 'sweetalert2';
-import {useRouter} from 'vue-router';
+import { useRouter } from 'vue-router';
 
 const queryset = ref([] as empresaInterface[])
 const url = useRouter()
+const loading = ref<boolean>(false)
+
 onMounted(async () => {
+  loading.value = true
   const response = await getCompany()
   queryset.value = response.data
+  loading.value = false
 })
 
 const onUpdate = async (data: empresaInterface) => {
@@ -43,7 +51,7 @@ const onUpdate = async (data: empresaInterface) => {
       showConfirmButton: false,
       timer: 1500
     })
-    url.push({name: 'empresa'})
+    url.push({ name: 'empresa' })
   }
 }
 </script>
