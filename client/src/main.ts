9e007329b837980e03357c472becfd5eb4ad041d
luchompi <lpichon63@gmail.com>
2023-10-23 16:49:19 -0500

    feat(Ver. 1.6.0.0): 🏗️Stocket - client
    UI: se han configurado dependencias para el proyecto nuevo

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,11 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 import App from './App.vue'
 import router from './router'
+import 'admin-lte/dist/css/adminlte.css'
+import 'admin-lte/dist/js/adminlte.js'
+import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
 
