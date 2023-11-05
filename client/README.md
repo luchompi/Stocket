1813ee1908491d8b9949701858a95a3223456670
luchompi <lpichon63@gmail.com>
2023-11-05 18:00:41 -0500

    feat(Ver. 1.0.1.0): :sparkles: Stocket - UI
    Profile app: se ha añadido formulario de creación de nuevos registros, implementando fbemitter

--- client/README.md
+++ client/README.md
@@ -6,3 +6,39 @@ Este documento tiene como finalidad definir los paquetes y cambios realizados en
 2. Se han instalado los paquetes react, react-router-dom, axios sweetalert2, admin-lte, lordicon, lottie-web
 3. Se ha implementado lordicon (https://lordicon.com/)
 4. Se ha implementado remix icons ([Remix Icon - Open source icon library](https://remixicon.com/))
+5. se ha implementado libreria fbemitter (https://www.youtube.com/watch?v=E1QbP1MWpMc&t=930s)
+
+¿como se usa fbemitter?
+
+para usarlo, 
+
+1. Se debe crear la instancia del emitter para solo invocarla una vez (helpers/EventEmitter.js)
+2. Creamos el componente hijo, importamos el event emitter y definimos el nombre del metodo que padre debe reconocer para recibir los datos y tambien los datos a enviar (views/profile/components/FormProfile.jsx , el disparador debe contener esto EventEmitter.emit("onSave", data);)
+3. El componente padre, debe usar un useEffect para observar los eventos y debe tener esta logica
+
+
+useEffect(() => {
+
+//onSave es el nombre del emitter definido en la vista, data los valores que viajan de hijo a padre
+
+const onSave = (data) => {
+
+//Logica aquí
+
+console.log(data);
+
+    };
+
+//Observador del listener, le permite al componente saber que está esperando, y que debe hacer apenas se dispare el evento, en este caso, espera "onSave" y se dirije a la funcion onSave dentro del hook
+
+const listener = EventEmitter.addListener("onSave", onSave);
+
+return () => {
+
+//Destructor del evento, así se borra todo al destruir el componente
+
+listener.remove();
+
+    };
+
+  }, []);
--- client/src/views/Profile/components/FormProfile.jsx
+++ client/src/views/Profile/components/FormProfile.jsx
@@ -1,29 +1,119 @@
 import React from "react";
-
+import EventEmitter from "../../../helpers/EventEmitter";
 const FormProfile = () => {
+  const [data, setData] = React.useState({
+    first_name: "",
+    last_name: "",
+    address: "",
+    phone: "",
+    city: "",
+    uid: "",
+  });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    EventEmitter.emit("onSave", data);
+  };
   return (
     <div>
-      <form className="form-inline">
-        <div className="input-group input-group-sm">
+      <form className="form-horizontal" onSubmit={handleSubmit}>
+        <div className="form-group">
+          {/*Ciudad */}
+          <label htmlFor="UID">
+            Identificación <i className="ri-hashtag"></i>
+          </label>
           <input
-            className="form-control form-control-navbar"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
+            type="text"
+            className="form-control form-control-border border-width-2"
+            id="UID"
+            placeholder="Especifique su identificación. Ej: 1234567890"
+            name="uid"
+            onChange={handleChange}
+            required
           />
-          <div className="input-group-append">
-            <button className="btn btn-navbar" type="submit">
-              <i className="fas fa-search"></i>
-            </button>
-            <button
-              className="btn btn-navbar"
-              type="button"
-              data-widget="navbar-search"
-            >
-              <i className="ri-arrow-right-fill"></i>
-            </button>
-          </div>
         </div>
+        <div className="form-group">
+          {/*Nombres */}
+          <label htmlFor="FirstName">
+            Nombres <i className="ri-account-box-line"></i>
+          </label>
+          <input
+            type="text"
+            className="form-control form-control-border border-width-2"
+            id="FirstName"
+            placeholder="Ingrese su nombre. Ej: pepe"
+            name="first_name"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          {/*Apellidos */}
+          <label htmlFor="LastName">
+            Apellidos <i className="ri-account-box-line"></i>
+          </label>
+          <input
+            type="text"
+            className="form-control form-control-border border-width-2"
+            id="LastName"
+            placeholder="Ingrese sus apellidos. Ej: perez"
+            name="last_name"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          {/*Ciudad */}
+          <label htmlFor="City">
+            Ciudad de residencia <i className="ri-crosshair-fill"></i>
+          </label>
+          <input
+            type="text"
+            className="form-control form-control-border border-width-2"
+            id="City"
+            placeholder="Ciudad de residencia. Ej: Bogotá DC"
+            name="city"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          {/*Dirección */}
+          <label htmlFor="Address">
+            Dirección de residencia <i className="ri-map-2-line"></i>
+          </label>
+          <input
+            type="text"
+            className="form-control form-control-border border-width-2"
+            id="Address"
+            placeholder="Defina dirección de residencia. Ej. Calle 123 # 123-123"
+            name="address"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          {/*Teléfono */}
+          <label htmlFor="Phone">
+            Teléfono de contacto <i className="ri-cellphone-line"></i>
+          </label>
+          <input
+            type="text"
+            className="form-control form-control-border border-width-2"
+            id="Phone"
+            placeholder="Defina su teléfono de contacto. Ej: 6057270000 ó 3177777777"
+            name="phone"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-success">
+          Enviar
+        </button>
       </form>
     </div>
   );
