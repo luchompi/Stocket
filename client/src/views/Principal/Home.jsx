6c7d96cc48da8eb6b12dc3f3aeaae7d9527c4ac5
luchompi <lpichon63@gmai.com>
2023-11-05 10:31:39 -0500

    style(Ver. 1.6.1.0): 🏗️Stocket - UI
    Se ha alterado comportamiento de la animación, ahora se importa desde un componente que automatiza la misma

--- client/src/views/Principal/Home.jsx
+++ client/src/views/Principal/Home.jsx
@@ -1,31 +1,27 @@
 import React from "react";
+import {IconHelperWitoutColors} from "../../helpers/IconHelper.jsx";
 
 const Home = () => {
-  document.title = "Stocket - Inicio";
-  return (
-    <div className="col col-lg-7">
-      <div className="card">
-        <div className="card-header">
-          <h3 className="card-title">Stocket</h3>
-        </div>
-        <div className="card-body" style={{ justifyContent: "center" }}>
-          <div className="row">
-            <div className="col">
-              <h4>Bienvenido a Stocket</h4>
-              <p>Su sistema de inventario TI</p>
+    document.title = "Stocket - Inicio";
+    return (
+        <div className="col col-lg-6">
+            <div className="card">
+                <div className="card-header">
+                    <h3 className="card-title">Stocket</h3>
+                </div>
+                <div className="card-body" style={{justifyContent: "center"}}>
+                    <div className="row">
+                        <IconHelperWitoutColors url={"https://cdn.lordicon.com/wegsbupf.json"}/>
+                        <div className="col">
+                            <h4>Bienvenido a Stocket</h4>
+                            <p>Su sistema de inventario TI</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="col">
-              <lord-icon
-                src="https://cdn.lordicon.com/wegsbupf.json"
-                trigger="loop"
-                style={{ width: "150px", height: "150px" }}
-              ></lord-icon>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    )
+        ;
 };
 
 export default Home;
