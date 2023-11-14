6219268df4c7a8fec0e22ddb87c48f8f9aa41ec9
Luis Pichon <71852050+luchompi@users.noreply.github.com>
2023-11-14 13:23:16 -0500

    feat(Ver. 1.0.1.0): :sparkles: Stocket -  UI
    Auth app: Vista de activacion de cuenta
    
    Se ha creado vista de activacion de cuentas

--- client/src/views/Auth/components/Activate.vue
+++ client/src/views/Auth/components/Activate.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
 import Animations from '@/components/Animations.vue';
 import { useRouter } from 'vue-router';
+import COMMS from '@/assets/Icons/COMMS.json';
+import useSesionStore from '@/stores/sesion.store'
+import { onMounted } from 'vue';
+const sesion = useSesionStore();
+const url = useRouter()
 
+const { uid, token } = url.currentRoute.value.params
 
+const activacion = () => {
+    var data = {
+        uid: uid,
+        token: token
+    }
+    sesion.activarPerfil(data)
+
+}
 </script>
 
 
 <template>
-    <div class="flex flex-col items-center justify-center h-screen">
-        <h1 class="text-3xl font-bold">Activate your account</h1>
-        <p class="text-gray-500">Please wait while we activate your account</p>
+    <div class="card-header">
+        <h6>Activación de cuenta</h6>
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <h5>Está a punto de activar su cuenta</h5>
+            <p>Para hacerlo, haga clic <button type="button" class="btn btn-success" @click="activacion">Aquí</button>
+            </p>
+        </div>
     </div>
 </template>
