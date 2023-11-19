9c7a20a4a83b6190b75cec031bca93e0ccadea66
luchompi <lpichon63@gmail.com>
2023-11-19 12:32:25 -0500

    refactor(Ver. 1.6.1.0): ⚰️ Stocket - UI
    Se ha eliminado codigo innecesario

--- server/apps/Empresa/apps.py
+++ server/apps/Empresa/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class EmpresaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.Empresa'
-
-    def ready(self) -> None:
-        from .signals import limitar_registros_empresa
-
