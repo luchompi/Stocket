4f756fd38ca84ae6bfdccbd8e8d80e6917f8f087
luchompi <lpichon63@gmail.com>
2024-01-26 09:51:28 -0500

    refactor(Ver. 1.6.1.0): :fire: Client
    Se ha eliminado proyecto en vuejs

--- client/src/views/Empresa/sede/services/sedes.interfaces.ts
+++
@@ -1,9 +0,0 @@
-export interface sedes {
-    id: number,
-    name: string,
-    address: string,
-    phone: string,
-    email: string,
-    description: string,
-    web: string
-}
--- client/src/views/Inventario/Referencias/services/services/referencias.interfaces.ts
+++
@@ -1,8 +0,0 @@
-export interface References{
-    id: string;
-    categoria:string;
-    marca:{
-        id: string;
-        marca_name:string;
-    }
-}
