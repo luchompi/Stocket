0c2a013be05f9a98baa64b28b94127e3ee2a84ce
luchompi <lpichon63@gmail.com>
2024-01-26 09:55:12 -0500

    chore(Ver. 1.6.1.0): :fire: Stocket
    Se han eliminado estaticos del servidor y mocks de prueba
    
    }

--- mocks/_public_._Empresa_empresa_ .sql
+++
@@ -1,2 +0,0 @@
-
-INSERT INTO "public"."Empresa_empresa" ("NIT", "name", "address", "phone", "email", "description", "web") VALUES ('2', 'Disney', 'Central Park', '12345', 'contact@disney.com', 'Disney is an animation company for children', 'http://www.disney.com');
