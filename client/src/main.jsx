1dc6802701ba7ec14fd13ac4e1eb6057452d07d0
luchompi <lpichon63@gmail.com>
2024-01-30 16:45:09 -0500

    feat(Ver. 1.6.1.0): :heavy_plus_sign: Client - remixicons
    Se ha añadido remixicons al proyecto

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "admin-lte/dist/css/adminlte.css";
 import "admin-lte/dist/js/adminlte.js";
 import "admin-lte/plugins/bootstrap/js/bootstrap.bundle.js";
+import "remixicon/fonts/remixicon.css";
 import { RouterProvider } from "react-router-dom";
 import Root from "./routes/Root";
 
